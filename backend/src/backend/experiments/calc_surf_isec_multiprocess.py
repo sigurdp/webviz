d3921291d4829d82c1e461cae0c3dcf93e89521e
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2023-10-12 15:44:46 +0200

    Experiment with using "spawn"

--- backend/src/backend/experiments/calc_surf_isec_multiprocess.py
+++ backend/src/backend/experiments/calc_surf_isec_multiprocess.py
@@ -3,8 +3,9 @@ import signal
 import numpy as np
 import logging
 from typing import List, Union, Optional
-from multiprocessing import Pool
-#from concurrent.futures import ProcessPoolExecutor
+import multiprocessing
+
+# from concurrent.futures import ProcessPoolExecutor
 from dataclasses import dataclass
 
 from src.services.sumo_access.surface_access import SurfaceAccess
@@ -15,21 +16,20 @@ from src.backend.utils.perf_metrics import PerfMetrics
 LOGGER = logging.getLogger(__name__)
 
 
-
-
 @dataclass
 class SurfItem:
-    #access_token: str
+    # access_token: str
     case_uuid: str
     ensemble_name: str
     name: str
     attribute: str
     real: int
-    #fence_arr: np.ndarray
+    # fence_arr: np.ndarray
+
 
 @dataclass
 class ResultItem:
-    perf_info:  str
+    perf_info: str
     line: np.ndarray
 
 
@@ -55,7 +55,7 @@ def process_a_surf(item: SurfItem) -> ResultItem:
     perf_metrics = PerfMetrics()
 
     access = global_access
-    #access = await SurfaceAccess.from_case_uuid(item.access_token, item.case_uuid, item.ensemble_name)
+    # access = await SurfaceAccess.from_case_uuid(item.access_token, item.case_uuid, item.ensemble_name)
     perf_metrics.record_lap("access")
 
     xtgeo_surf = access.get_realization_surface_data_sync(real_num=item.real, name=item.name, attribute=item.attribute)
@@ -63,7 +63,7 @@ def process_a_surf(item: SurfItem) -> ResultItem:
         return None
     perf_metrics.record_lap("fetch")
 
-    #line = xtgeo_surf.get_randomline(item.fence_arr)
+    # line = xtgeo_surf.get_randomline(item.fence_arr)
     line = xtgeo_surf.get_randomline(global_fence_arr)
     perf_metrics.record_lap("calc")
 
@@ -84,7 +84,6 @@ async def calc_surf_isec_multiprocess(
     num_reals: int,
     cutting_plane: schemas.CuttingPlane,
 ) -> List[schemas.SurfaceIntersectionData]:
-
     myprefix = ">>>>>>>>>>>>>>>>> calc_surf_isec_multiprocess():"
     print(f"{myprefix} started", flush=True)
 
@@ -92,30 +91,44 @@ async def calc_surf_isec_multiprocess(
         [cutting_plane.x_arr, cutting_plane.y_arr, np.zeros(len(cutting_plane.y_arr)), cutting_plane.length_arr]
     ).T
 
-    access_token=authenticated_user.get_sumo_access_token()
+    access_token = authenticated_user.get_sumo_access_token()
 
     item_list = []
     for i in range(num_reals):
-        item_list.append(SurfItem(
-            #access_token=access_token,
-            case_uuid=case_uuid,
-            ensemble_name=ensemble_name,
-            name=name,
-            attribute=attribute,
-            real=i,
-            #fence_arr=fence_arr
-        ))
+        item_list.append(
+            SurfItem(
+                # access_token=access_token,
+                case_uuid=case_uuid,
+                ensemble_name=ensemble_name,
+                name=name,
+                attribute=attribute,
+                real=i,
+                # fence_arr=fence_arr
+            )
+        )
 
     print(f"{myprefix} built item_list {len(item_list)=}", flush=True)
 
     intersections = []
-    with Pool(initializer=init_access_and_fence, initargs=(access_token, case_uuid, ensemble_name, fence_arr)) as pool:
+
+    # Experiment with switching to spawn
+    # Note that for multiprocess the default is fork
+    # See: https://superfastpython.com/multiprocessing-pool-context/
+    context_object = multiprocessing.get_context("spawn")
+
+    with multiprocessing.Pool(
+        context=context_object,
+        initializer=init_access_and_fence,
+        initargs=(access_token, case_uuid, ensemble_name, fence_arr),
+    ) as pool:
         res_item_arr = pool.map(process_a_surf, item_list)
         print(f"{myprefix} back from map {len(res_item_arr)=}", flush=True)
 
         for res_item in res_item_arr:
             if res_item is not None and res_item.line is not None:
-                isecdata = schemas.SurfaceIntersectionData(name="someName", hlen_arr=res_item.line[:, 0].tolist(), z_arr=res_item.line[:, 1].tolist())
+                isecdata = schemas.SurfaceIntersectionData(
+                    name="someName", hlen_arr=res_item.line[:, 0].tolist(), z_arr=res_item.line[:, 1].tolist()
+                )
                 intersections.append(isecdata)
                 print(f"{myprefix} got isec {len(intersections)}  perf_info={res_item.perf_info}", flush=True)
             else:
@@ -124,5 +137,3 @@ async def calc_surf_isec_multiprocess(
     print(f"{myprefix} finished", flush=True)
 
     return intersections
-
-
